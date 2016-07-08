2cdd6a6903ce2e0e2ab0703be0b2003e50700fe0
Florian Meier <florian.meier@koalo.de>
2016-07-08 17:11:49 +0200

    Run test simulation during CI

--- ci_test.sh
+++ ci_test.sh
@@ -4,7 +4,7 @@ export RUN=5
 export REP=0
 
 function run {
-    opp_run -r $RUN --seed-set=$REP --repeat=1 --cmdenv-express-mode=false --vector-recording=true -u Cmdenv -c $1 -n simulations:src:$INET/examples:$INET/src -l $INET/src/INET -l src/inet-dsme --debug-on-errors=false simulations/omnetpp.ini > $1-mac.log
+    opp_run -r $RUN --seed-set=$REP --repeat=1 --**.mac.cmdenv-log-level=info --cmdenv-express-mode=false --cmdenv-status-frequency=6s --result-dir=results --vector-recording=true -u Cmdenv -c $1 -n simulations:src:$INET/examples:$INET/src -l $INET/src/INET -l src/inet-dsme --debug-on-errors=false simulations/omnetpp.ini | tee results/$1-mac.log | awk 'NR < 20 || NR % 100000 == 0'
 }
 
 run DSME &
