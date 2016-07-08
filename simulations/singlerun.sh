93ab9cc69c3336dee51fffdbb0c2338d4a4f1095
Florian Kauer <florian.kauer@koalo.de>
2016-07-08 18:20:34 +0200

    Enable logging

--- simulations/singlerun.sh
+++ simulations/singlerun.sh
@@ -4,10 +4,11 @@ pushd ../
 make || exit 1
 popd
 
-RUN=7
-REP=4
+CONFIG=DSME
+RUN=4
+REP=0
 VECTOR_RECORDING=true
 #VECTOR_RECORDING=false
 
-opp_run -r $RUN --seed-set=$REP --repeat=1 --cmdenv-express-mode=false --vector-recording=$VECTOR_RECORDING -u Cmdenv -c DSME -n .:../src:../../inet/examples:../../inet/src:../../inet/tutorials:.:../src -l ../../inet/src/INET -l ../src/inet-dsme -l ../../inet/src/INET -l ../src/inet-dsme -l ../../inet/src/INET -l ../src/inet-dsme --debug-on-errors=false omnetpp.ini > mac.log
+opp_run -r $RUN --seed-set=$REP --repeat=1 --cmdenv-express-mode=false --vector-recording=$VECTOR_RECORDING -u Cmdenv -c $CONFIG -n .:../src:../../inet/examples:../../inet/src:../../inet/tutorials:.:../src -l ../../inet/src/INET -l ../src/inet-dsme -l ../../inet/src/INET -l ../src/inet-dsme -l ../../inet/src/INET -l ../src/inet-dsme --debug-on-errors=false omnetpp.ini > mac.log
 
