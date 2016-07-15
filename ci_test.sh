21215e1cf1a1d12d7d817b45a3985d832ded8294
Florian Kauer <florian.kauer@koalo.de>
2016-07-15 12:40:38 +0200

    Separate build script

--- ci_test.sh
+++ ci_test.sh
@@ -3,6 +3,8 @@ export INET=$1
 export RUN=5
 export REP=0
 
+mkdir -p results
+
 function run {
     opp_run -r $RUN --seed-set=$REP --repeat=1 --**.mac.cmdenv-log-level=info --cmdenv-express-mode=false --cmdenv-status-frequency=6s --result-dir=results --vector-recording=true -u Cmdenv -c $1 -n simulations:src:$INET/examples:$INET/src -l $INET/src/INET -l src/inet-dsme --debug-on-errors=false simulations/omnetpp.ini | tee results/$1-mac.log | awk 'NR < 20 || NR % 100000 == 0'
 }
