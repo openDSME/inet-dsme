49ef1768cc7caeca2529f528b6b328f8d0c50a5f
Maximilian Köstler <maximilian.koestler@tuhh.de>
2016-08-22 14:37:17 +0200

    Decrease macResponseWaitTime to 16.

--- src/DSMEPlatform.cc
+++ src/DSMEPlatform.cc
@@ -159,6 +159,7 @@ void DSMEPlatform::initialize(int stage) {
         this->mac_pib.macMaxFrameRetries = par("macMaxFrameRetries");
 
         this->mac_pib.macDSMEGTSExpirationTime = 64;
+        this->mac_pib.macResponseWaitTime = 16;
 
         this->mac_pib.recalculateDependentProperties();
 
