873c29f8c7387a37b3354b9f0bfa3c328d746147
Maximilian Köstler <maximilian.koestler@tuhh.de>
2016-08-23 13:41:02 +0200

    Simulate lower send intervals.

--- simulations/singlerun.sh
+++ simulations/singlerun.sh
@@ -4,11 +4,11 @@ pushd ../
 make || exit 1
 popd
 
-CONFIG=DSME-Multi
+CONFIG=DSME
 RUN=0
 REP=0
-VECTOR_RECORDING=true
-#VECTOR_RECORDING=false
+#VECTOR_RECORDING=true
+VECTOR_RECORDING=false
 
 opp_run -r $RUN --seed-set=$REP --repeat=1 --cmdenv-express-mode=false --vector-recording=$VECTOR_RECORDING -u Cmdenv -c $CONFIG -n .:../src:../../inet/examples:../../inet/src:../../inet/tutorials:.:../src -l ../../inet/src/INET -l ../src/inet-dsme -l ../../inet/src/INET -l ../src/inet-dsme -l ../../inet/src/INET -l ../src/inet-dsme --debug-on-errors=false omnetpp.ini > mac.log
 
