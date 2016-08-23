b79c7093697bcd21a77cb3942d9b5c981003c5f3
Maximilian Köstler <maximilian.koestler@tuhh.de>
2016-08-23 14:36:25 +0200

    Improve GTS evaluation script.

--- utils/gts_allocation_time.py
+++ utils/gts_allocation_time.py
@@ -19,11 +19,15 @@ def printMatrix(a):
 parser = argparse.ArgumentParser(description="Extracts the number of allocated and deallocated slots per second.")
 parser.add_argument("-l", "--log", type=str, required=True, help="the log file to parse")
 parser.add_argument("-o", "--output", type=str, default="gts_allocation.csv", help="the output file")
+parser.add_argument("-s", "--step", type=str, default="1", help="unit of time")
 args = parser.parse_args()
 
-allocationVector = numpy.zeros((500,2))
+allocationVector = numpy.zeros((500,3))
 totalAlloc = 0
 totalDealloc = 0
+
+for i in range(0,500):
+    allocationVector[i][0] = i * int(args.step)
 
 for line in open(args.log):
     m = re.search("^\[\w*\]\s*([0-9.]*)\s*[0-9]*: ((de)?)alloc ([0-9]+)(.)([0-9]+) ([0-9]+),([0-9]+),([0-9]+)", line)
@@ -34,18 +38,18 @@ for line in open(args.log):
             source = int(m.group(4)) - 1
             destination = int(m.group(6)) - 1
             time = float(m.group(1))
-            pos = math.floor(time)
+            pos = math.floor(math.floor(time) / int(args.step))
             if m.group(2) == 'de':
                 totalDealloc += 1
-                allocationVector[pos][0] += 1
+                allocationVector[pos][1] += 1
             else:
                 totalAlloc += 1
-                allocationVector[pos][1] += 1
+                allocationVector[pos][2] += 1
 
 
 print "Data collection finished"
 
-printMatrix(allocationVector)
+#printMatrix(allocationVector)
 numpy.savetxt(args.output, allocationVector, delimiter=";")
 
 print "Dealloc %i  Alloc %i\n"%(totalDealloc,totalAlloc)
