1cc07d2f4a671b16007d190b4617402b55620bb8
Florian Kauer <florian.kauer@koalo.de>
2016-08-23 15:30:39 +0200

    Constrain test run
    Thus much lower rates can be tested without destroying the Travis build
    
    Signed-off-by: Florian Kauer <florian.kauer@koalo.de>

--- ci_test.sh
+++ ci_test.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 export INET=$1
-export RUN=5
+export RUN=0
 export REP=0
+export CONSTRAINT="\$sendInt>=2"
 
 mkdir -p results
 
 function run {
-    opp_run -r $RUN --seed-set=$REP --repeat=1 --**.mac.cmdenv-log-level=info --cmdenv-express-mode=false --cmdenv-status-frequency=6s --result-dir=../results --vector-recording=true -u Cmdenv -c $1 -n simulations:src:$INET/examples:$INET/src -l $INET/src/INET -l src/inet-dsme --debug-on-errors=false simulations/omnetpp.ini | tee results/$1-mac.log | awk 'NR < 20 || NR % 100000 == 0'
+    opp_run -r $RUN --constraint="$CONSTRAINT" --seed-set=$REP --repeat=1 --**.mac.cmdenv-log-level=info --cmdenv-express-mode=false --cmdenv-status-frequency=6s --result-dir=../results --vector-recording=true -u Cmdenv -c $1 -n simulations:src:$INET/examples:$INET/src -l $INET/src/INET -l src/inet-dsme --debug-on-errors=false simulations/omnetpp.ini | tee results/$1-mac.log | awk 'NR < 20 || NR % 100000 == 0'
 }
 
 run DSME &
