c37ab19eaf33a5555b0e58de4a80cc1618a3da38
Florian Kauer <florian.kauer@koalo.de>
2016-10-07 16:36:53 +0200

    Correcting header

--- src/DSMEPlatform.h
+++ src/DSMEPlatform.h
@@ -1,42 +1,45 @@
 /*
+ * openDSME
+ *
  * Implementation of the Deterministic & Synchronous Multi-channel Extension (DSME)
  * introduced in the IEEE 802.15.4e-2012 standard
  *
  * Authors: Florian Meier <florian.meier@tuhh.de>
- *          Institute of Telematics, Hamburg University of Technology
- *          Copyright 2015
+ *          Maximilian Koestler <maximilian.koestler@tuhh.de>
+ *          Sandrina Backhauss <sandrina.backhauss@tuhh.de>
  *
  * Based on
- *  DSME Implementation for the INET Framework
- *  Tobias Luebkert <tobias.luebkert@tuhh.de>
- *  Institute of Telematics, Hamburg University of Technology
+ *          DSME Implementation for the INET Framework
+ *          Tobias Luebkert <tobias.luebkert@tuhh.de>
  *
+ * Copyright (c) 2015, Institute of Telematics, Hamburg University of Technology
+ * All rights reserved.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License Version 3
- * or later (the "LGPL").
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
  *
- * As a special exception, the copyright holders of this library give
- * you permission to link this library with independent modules to produce an
- * executable, regardless of the license terms of these independent modules,
- * and to copy and distribute the resulting executable under terms of your
- * choice, provided that you also meet, for each linked independent module,
- * the terms and conditions of the license of that module.
- * An independent module is a module which is not derived from or based
- * on this library.
- * If you modify this library, you may extend this exception to your version
- * of the library, but you are not obligated to do so. If you do not wish to
- * do so, delete this exception statement from your version.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * and the GNU Lesser General Public License along with this program.
- * If not, see <http://www.gnu.org/licenses/>.
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
  */
+
 #ifndef DSMEPLATFORM_H
 #define DSMEPLATFORM_H
 
