e53b68a01c3fe67ce0dc0374200ddd6df8dd3cd6
Florian Kauer <florian.kauer@koalo.de>
2017-02-14 14:08:47 +0100

    Fix analyze for single run

--- utils/analyze.py
+++ utils/analyze.py
@@ -16,7 +16,10 @@ from sets import Set
 def mean_confidence_interval(data, confidence=0.95):
     a = 1.0*np.array(data)
     n = len(a)
-    m, se = np.mean(a), scipy.stats.sem(a)
+    m = np.mean(a)
+    se = 0
+    if len(a) > 1:
+        se = scipy.stats.sem(a)
     h = se * sp.stats.t._ppf((1+confidence)/2., n-1)
     return m, h
 
