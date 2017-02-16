6469a402c53cd62249c53fc2be4d6d24dda5717e
Florian Kauer <florian.kauer@koalo.de>
2017-02-16 13:41:19 +0100

    Allow for arbitrary parameters

--- utils/analyze.py
+++ utils/analyze.py
@@ -30,7 +30,7 @@ def main(args):
     aggregated = {}
     runs = []
     repetitions = Set()
-    for f in glob.glob(args.results+'/*.sca'):
+    for f in args.input:
         r = Run()
         r.load(f)
 
@@ -40,10 +40,10 @@ def main(args):
         
         repetitions.add(r.param["seedset"])
         for m in measures:
-            aggregated.setdefault(r.param["configname"],{}).setdefault(r.param["sendInt"],{}).setdefault(m,{})[r.param["seedset"]] = r.measure[m]
+            aggregated.setdefault(r.param["configname"],{}).setdefault(r.param[args.parameter],{}).setdefault(m,{})[r.param["seedset"]] = r.measure[m]
 
-    with open(args.results+'/aggregated.csv','w') as resultfile:
-        cols = ['config','sendInt']
+    with open(args.output+'/aggregated.csv','w') as resultfile:
+        cols = ['config',args.parameter]
         for m in measures:
             cols += [m, m+"_error"]
 
@@ -52,13 +52,12 @@ def main(args):
         writer.writerow(headers)
 
         for config,v in aggregated.iteritems():
-            for sendInt in sorted(v.keys()):
+            for param in sorted(v.keys()):
                 result = { 'config': config,
-                                  'sendInt': sendInt,
+                                  args.parameter: param,
                                   }
                 for k in measures: 
-                    #print v[sendInt]
-                    (m,h) = mean_confidence_interval(v[sendInt][k].values())
+                    (m,h) = mean_confidence_interval(v[param][k].values())
 
                     if np.isnan(m):# TODO remove
                         m = 0
@@ -68,14 +67,15 @@ def main(args):
                     result[k] = m
                     result[k+"_error"] = h
                 writer.writerow(result)
+                print result
 
-    with open(args.results+'/per_host.csv','w') as resultfile:
+    with open(args.output+'/per_host.csv','w') as resultfile:
         sets = set()
         
         perhost = {}
         for r in runs:
             for host in allhosts:
-                setname = "%s-%s-%s-"%(r.param['configname'],r.param['sendInt'],r.param['seedset'])
+                setname = "%s-%s-%s-"%(r.param['configname'],r.param[args.parameter],r.param['seedset'])
                 sets.add(setname+"received")
                 sets.add(setname+"lost")
                 if r.hosts.has_key(host):
@@ -90,7 +90,7 @@ def main(args):
 
         cols = ['address']
         cols += sorted(sets)
-        print cols
+        #print cols
 
         writer = csv.DictWriter(resultfile, fieldnames=cols)
         headers = dict( (n,n) for n in cols )
@@ -103,7 +103,9 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('results', help='Directory with result files')
+    parser.add_argument('-p','--parameter', help="Parameter of x-Axis", default='sendInt')
+    parser.add_argument('output', help='Output directory')
+    parser.add_argument('input', help='Input scalar files', nargs='+')
     args = parser.parse_args()
 
     main(args)
