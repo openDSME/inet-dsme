4ada7ff22fb88e908301118d50917c3408e125ec
Maximilian Köstler <maximilian.koestler@tuhh.de>
2017-03-02 15:54:18 +0100

    Default values for DSME parameters

--- simulations/singlerun.sh
+++ simulations/singlerun.sh
@@ -4,11 +4,10 @@ pushd ../
 make -j8 || exit 1
 popd
 
-CONFIG=DSME-Multi
+CONFIG=DSME
 RUN=5
 REP=2
-VECTOR_RECORDING=true
-#VECTOR_RECORDING=false
+VECTOR_RECORDING=false
 
 opp_run -r $RUN --seed-set=$REP --repeat=1 --cmdenv-express-mode=false --vector-recording=$VECTOR_RECORDING -u Cmdenv -c $CONFIG -n .:../src:../../inet/examples:../../inet/src:../../inet/tutorials:.:../src -l ../../inet/src/INET -l ../src/inet-dsme -l ../../inet/src/INET -l ../src/inet-dsme -l ../../inet/src/INET -l ../src/inet-dsme --debug-on-errors=false omnetpp.ini > mac.log
 
