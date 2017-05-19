077927f66a000c3ec9f2f55824f364a71cfbfe4e
Florian Kauer <florian.kauer@koalo.de>
2017-05-19 17:24:03 +0200

    Typo

--- build.sh
+++ build.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+then 
+	echo "Call it as "
+	echo "./build.sh inet-path"
+	exit 1
+fi
+
 export INET=`readlink -f $1`
 echo $INET
 pushd src && opp_makemake -f --deep --make-so -I$INET/src -I$INET/src/inet/common -I.. -KINET_PROJ=$INET && popd
