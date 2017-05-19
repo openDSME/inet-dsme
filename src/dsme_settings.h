4d3f6ea7de29bd7e53cbd06c93346dfba8508968
Florian Kauer <florian.kauer@koalo.de>
2017-05-19 10:51:15 +0200

    Add Additional ACK Wait Duration of 0

--- src/dsme_settings.h
+++ src/dsme_settings.h
@@ -72,6 +72,7 @@ constexpr uint16_t CAP_QUEUE_SIZE = 8;
 constexpr uint16_t TOTAL_GTS_QUEUE_SIZE = 30 - CAP_QUEUE_SIZE;
 constexpr uint16_t UPPER_LAYER_QUEUE_SIZE = 12;
 constexpr uint16_t MSG_POOL_SIZE = CAP_QUEUE_SIZE + TOTAL_GTS_QUEUE_SIZE + 2 * UPPER_LAYER_QUEUE_SIZE + 10;
+constexpr uint8_t ADDITIONAL_ACK_WAIT_DURATION = 0;
 }
 
 #endif
