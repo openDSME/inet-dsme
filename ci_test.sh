c32a3a6d95dc5823d60decac19025e5f361c96e5
Florian Kauer <florian.kauer@koalo.de>
2018-04-03 12:46:54 +0200

    Debugging Travis

--- ci_test.sh
+++ ci_test.sh
@@ -7,7 +7,7 @@ export CONSTRAINT="\$sendInt>=2"
 mkdir -p results
 
 function run {
-    opp_run -r $RUN --constraint="$CONSTRAINT" --seed-set=$REP --repeat=1 --**.mac.cmdenv-log-level=info --cmdenv-express-mode=false --cmdenv-status-frequency=6s --result-dir=../results --vector-recording=true -u Cmdenv -c $1 -n simulations:src:$INET/examples:$INET/src -l $INET/src/INET -l src/inet-dsme --debug-on-errors=false simulations/multi_gateway.ini | tee results/$1-mac.log | awk 'NR < 20 || NR % 100000 == 0'
+    opp_run -r $RUN --constraint="$CONSTRAINT" --seed-set=$REP --repeat=1 --**.mac.cmdenv-log-level=info --cmdenv-express-mode=false --cmdenv-status-frequency=6s --result-dir=../results --vector-recording=true -u Cmdenv -c $1 -n simulations:src:$INET/examples:$INET/src -l $INET/src/INET -l src/inet-dsme --debug-on-errors=false simulations/evaluation.ini | tee results/$1-mac.log | awk 'NR < 20 || NR % 100000 == 0'
 }
 
 run DSME &
