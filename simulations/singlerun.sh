33b6943be3a7dead5a3815e56fddd95243d655c9
Florian Meyer <fl.meyer@tuhh.de>
2020-01-24 12:51:08 +0100

    fixed project settings

--- simulations/singlerun.sh
+++ simulations/singlerun.sh
@@ -10,5 +10,5 @@ REP=0
 VECTOR_RECORDING=true
 #VECTOR_RECORDING=false
 
-opp_run -r $RUN --seed-set=$REP --repeat=1 --cmdenv-express-mode=false --vector-recording=$VECTOR_RECORDING -u Cmdenv -c $CONFIG -n .:../src:../../inet/examples:../../inet/src:../../inet/tutorials:.:../src -l ../../inet/src/INET -l ../src/inet-dsme -l ../../inet/src/INET -l ../src/inet-dsme -l ../../inet/src/INET -l ../src/inet-dsme --debug-on-errors=false example.ini > mac.log
+opp_run -r $RUN --seed-set=$REP --repeat=1 --cmdenv-express-mode=false --vector-recording=$VECTOR_RECORDING -u Qtenv -c $CONFIG -n .:../src:../../inet/examples:../../inet/src:../../inet/tutorials:.:../src -l ../../inet/src/INET -l ../src/inet-dsme -l ../../inet/src/INET -l ../src/inet-dsme -l ../../inet/src/INET -l ../src/inet-dsme --debug-on-errors=false example.ini > mac.log
 
